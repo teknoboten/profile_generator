34b4e7f959e3de211c9fe539d88daf788d35bfcf
modemgirl <serra@modemgirl.ca>
2022-02-19 18:06:05 +0000

    survey is working, but maybe i'm going to break it now

--- survey.js
+++ survey.js
@@ -1,29 +1,48 @@
-const readline = require('readline'); // require node readline module 
+const readline = require('readline'); // require node readline module
 const { stdin: input, stdout: output } = require('process');  //capture input / send output from process object
 
 const rl = readline.createInterface({ input, output });
-//readline createInterface method sends prompts for user input to stdout 
-//reads user input from stdin 
+//readline createInterface method sends prompts for user input to stdout
+//reads user input from stdin
 //createInterface is a constructor class!
 
-rl.question(`hello, what is your name? (or nickname?) `, (answer1) => {
-  console.log(`hi, ${answer1}, nice to meet you!`);
 
-  rl.question('so..what do you eat for breakfast? ', (answer2) => {
-    console.log(`mmm... ${answer2} sounds tasty.`);
+let profile = {}
 
-    rl.question(`what do you do for fun? `, (answer3) => {
-      console.log(`cool, i like ${answer3} too!`);
-    
+rl.question(`hello, what is your name? (or nickname?) : `, (answer1) => {
+  profile.name = answer1;
+  console.log(`hi, ${answer1}, nice to meet you! \n`);
 
-    rl.close();
-    
+  rl.question('so..what do you eat for breakfast? : ', (answer2) => {
+    console.log(`mmm... ${answer2} sounds tasty. \n`);
+
+    rl.question(`what do you do for fun? : `, (answer3) => {
+      console.log(`cool, i like ${answer3} too! \n`);
+      
+      rl.question(`what's the last band / show you went to see? : `, (answer4) => {
+        console.log(`oh yeah, i like ${answer4}'s older stuff  \n`);
+      
+        rl.question(`what is your biggest fear? : `, (answer5) => {
+          console.log(`yeah, ${answer5} is too scary for me, too. \n`);
+
+          rl.question(`what is your super power? : `, (answer4) => {
+            console.log(`that's great. the world needs more people like you, ${answer1}.  \n`);
+
+            rl.close();
+
+          });
+
+        });
+  
+      });
+
     });
 
   });
 
 });
 
+// console.log(profile);
 
 
 
